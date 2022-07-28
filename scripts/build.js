758e885bd32a7a653db36b6c4aebd0755d58daf7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-07-28 13:02:50 +0200

    fix build script (#3)

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process'
-import fs from 'fs'
+import fs, { readFileSync } from 'fs'
 import path, { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 import { packageExtension } from '@lvce-editor/package-extension'
@@ -24,23 +24,39 @@ const NOT_NEEDED = [
   'node_modules/prettier/standalone.js',
   'node_modules/prettier/third-party.js',
 ]
-
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const root = path.join(__dirname, '..')
+const extension = path.join(root, 'packages', 'extension')
 
 fs.rmSync(join(root, 'dist'), { recursive: true, force: true })
 
 fs.mkdirSync(path.join(root, 'dist'))
 
-fs.copyFileSync(join(root, 'package.json'), join(root, 'dist', 'package.json'))
+const packageJson = JSON.parse(
+  readFileSync(join(extension, 'package.json')).toString()
+)
+delete packageJson.xo
+delete packageJson.jest
+delete packageJson.prettier
+delete packageJson.devDependencies
+
+fs.writeFileSync(
+  join(root, 'dist', 'package.json'),
+  JSON.stringify(packageJson, null, 2) + '\n'
+)
 fs.copyFileSync(join(root, 'README.md'), join(root, 'dist', 'README.md'))
-fs.copyFileSync(join(root, 'icon.png'), join(root, 'dist', 'icon.png'))
+fs.copyFileSync(join(extension, 'icon.png'), join(root, 'dist', 'icon.png'))
 fs.copyFileSync(
-  join(root, 'extension.json'),
+  join(extension, 'extension.json'),
   join(root, 'dist', 'extension.json')
 )
-fs.cpSync(join(root, 'src'), join(root, 'dist', 'src'), { recursive: true })
+fs.cpSync(join(extension, 'src'), join(root, 'dist', 'src'), {
+  recursive: true,
+})
+fs.cpSync(join(extension, 'data'), join(root, 'dist', 'data'), {
+  recursive: true,
+})
 
 const getAllDependencies = (obj) => {
   if (!obj || !obj.dependencies) {
@@ -51,7 +67,7 @@ const getAllDependencies = (obj) => {
 
 const getDependencies = () => {
   const stdout = execSync('npm list --omit=dev --parseable --all', {
-    cwd: root,
+    cwd: extension,
   }).toString()
   const lines = stdout.split('\n')
   return lines.slice(1, -1)
@@ -59,13 +75,17 @@ const getDependencies = () => {
 
 const dependencies = getDependencies()
 for (const dependency of dependencies) {
-  fs.cpSync(dependency, join(root, 'dist', dependency.slice(root.length)), {
-    recursive: true,
-  })
+  fs.cpSync(
+    dependency,
+    join(root, 'dist', dependency.slice(extension.length)),
+    {
+      recursive: true,
+    }
+  )
 }
 
 for (const notNeeded of NOT_NEEDED) {
-  fs.rmSync(join(root, 'dist', notNeeded), { force: true })
+  fs.rmSync(join(root, 'dist', notNeeded), { force: true, recursive: true })
 }
 
 await packageExtension({
