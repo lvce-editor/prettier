fe9c78aeb368acb14b92297802f3f20d3eb69af8
Le Vivilet <levivlet@pm.me>
2022-08-19 15:15:47 +0200

    refactor e2e tests and update dependencies

--- packages/e2e/src/prettier.format-html.js
+++ packages/e2e/src/prettier.format-html.js
@@ -1,19 +1,7 @@
-import {
-  getTmpDir,
-  runWithExtension,
-  test,
-} from '@lvce-editor/test-with-playwright'
-import { writeFile } from 'fs/promises'
-import { join } from 'node:path'
-
-test('prettier.format-html', async (t) => {
-  const tmpDir = await getTmpDir()
-  await writeFile(join(tmpDir, 'test.html'), `<h1> hello world </h1>`)
-  const page = await runWithExtension({
-    folder: tmpDir,
-  })
-  const testHtml = page.locator('text=test.html')
-  await testHtml.click()
+test('prettier.format-html', async () => {
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.html`, `<h1> hello world </h1>`)
+  await Main.openUri(`${tmpDir}/test.html`)
 
   // TODO test that formatting works as expected
 })
