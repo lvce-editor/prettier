22d14b7ba697146261412eff23ae4c4fd3834afa
Le Vivilet <levivlet@pm.me>
2022-11-11 12:07:49 +0100

    active package for more language ids

--- packages/benchmark/index.js
+++ packages/benchmark/index.js
@@ -1,9 +1,12 @@
 import * as Format from '../extension/src/parts/Format/Format.js'
-
+const code = `a {
+  color: red;
+}
+`
 const main = async () => {
-  await Format.format('/test/file.css', "h1{};'dedw ")
+  await Format.format('/test/file.css', code)
   console.time('format')
-  await Format.format('/test/file.css', 'h1{} ')
+  await Format.format('/test/file.css', code)
   console.timeEnd('format')
 }
 
