eb8dde9dc68b5c7b73b8031d7fdf79ce7d999cc6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-28 13:22:47 +0100

    update dependencies (#30)

--- scripts/build-static.js
+++ scripts/build-static.js
@@ -1,6 +1,12 @@
 import { exportStatic } from '@lvce-editor/shared-process'
+import path, { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const root = path.join(__dirname, '..')
 
 await exportStatic({
   extensionPath: 'packages/extension',
   testPath: 'packages/e2e',
+  root,
 })
