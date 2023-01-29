9a9f0d95ae5a2218c2dd8afeb725f51fc754edee
Le Vivilet <levivlet@pm.me>
2023-01-29 12:43:11 +0100

    fix: third party prettier module missing in static build

--- scripts/build-static.js
+++ scripts/build-static.js
@@ -1,4 +1,5 @@
 import { exportStatic } from '@lvce-editor/shared-process'
+import { cp, readdir } from 'node:fs/promises'
 import path, { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
@@ -10,3 +11,24 @@ await exportStatic({
   testPath: 'packages/e2e',
   root,
 })
+
+const RE_COMMIT_HASH = /^[a-z\d]+$/
+const isCommitHash = (dirent) => {
+  return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
+}
+
+const dirents = await readdir(path.join(root, 'dist'))
+const commitHash = dirents.find(isCommitHash) || ''
+
+await cp(
+  path.join(root, 'packages', 'extension', 'third_party'),
+  path.join(
+    root,
+    'dist',
+    commitHash,
+    'extensions',
+    'builtin.prettier',
+    'third_party'
+  ),
+  { recursive: true, force: true }
+)
