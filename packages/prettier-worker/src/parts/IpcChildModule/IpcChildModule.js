4633007b0d77bbb398d14785f4e93104fe8baf05
Le Vivilet <levivlet@pm.me>
2023-02-09 23:42:43 +0100

    fix formatting on firefox

--- packages/prettier-worker/src/parts/IpcChildModule/IpcChildModule.js
+++ packages/prettier-worker/src/parts/IpcChildModule/IpcChildModule.js
@@ -4,6 +4,8 @@ export const getModule = (method) => {
   switch (method) {
     case IpcChildType.ModuleWorker:
       return import('../IpcChildWithModuleWorker/IpcChildWithModuleWorker.js')
+    case IpcChildType.MessagePort:
+      return import('../IpcChildWithMessagePort/IpcChildWithMessagePort.js')
     default:
       throw new Error('unexpected ipc type')
   }
---
+++ packages/prettier-worker/src/parts/IpcChildWithMessagePort/IpcChildWithMessagePort.js
@@ -0,0 +1,19 @@
+export const listen = () => {
+  const messageChannel = new MessageChannel()
+  const { port1, port2 } = messageChannel
+  globalThis.acceptPort(port2)
+  return {
+    send(message) {
+      port1.postMessage(message)
+    },
+    sendAndTransfer(message, transferables) {
+      port1.postMessage(message, transferables)
+    },
+    get onmessage() {
+      return port1.onmessage
+    },
+    set onmessage(listener) {
+      port1.onmessage = listener
+    },
+  }
+}
