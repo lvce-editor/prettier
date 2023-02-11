502c6220d2fe6522970c67d075b18e96281a59a1
Le Vivilet <levivlet@pm.me>
2023-02-11 13:48:32 +0100

    fix commands

--- packages/extension/src/parts/Format/Format.js
+++ packages/extension/src/parts/Format/Format.js
@@ -1,7 +1,8 @@
 import * as PrettierWorker from '../PrettierWorker/PrettierWorker.js'
+import * as PrettierWorkerCommandType from '../PrettierWorkerCommandType/PrettierWorkerCommandType.js'
 
 export const format = async (uri, text) => {
   const rpc = await PrettierWorker.getInstance()
-  const result = await rpc.invoke('Prettier.format', uri, text)
+  const result = await rpc.invoke(PrettierWorkerCommandType.Format, uri, text)
   return result
 }
