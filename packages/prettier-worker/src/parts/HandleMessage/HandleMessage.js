fdb1907d3ec1f03c3931043246037e15f3e27cd0
Le Vivilet <levivlet@pm.me>
2023-03-12 19:52:29 +0100

    fix e2e tests

--- packages/prettier-worker/src/parts/HandleMessage/HandleMessage.js
+++ packages/prettier-worker/src/parts/HandleMessage/HandleMessage.js
@@ -2,11 +2,6 @@ import * as GetResponse from '../GetResponse/GetResponse.js'
 
 export const handleMessage = async (event) => {
   const message = event.data
-  const { method, params, id } = message
-  const result = await GetResponse.getResponse(method, params)
-  event.target.postMessage({
-    jsonrpc: '2.0',
-    id: id,
-    result: result,
-  })
+  const response = await GetResponse.getResponse(message)
+  event.target.postMessage(response)
 }
