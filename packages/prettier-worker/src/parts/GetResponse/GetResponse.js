8c6e34be7ce409498f546c3bcc6c6e7e6f4865a9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-12 19:54:52 +0100

    refactor: improve error handling in prettier-worker (#49)

--- packages/prettier-worker/src/parts/GetResponse/GetResponse.js
+++ packages/prettier-worker/src/parts/GetResponse/GetResponse.js
@@ -1,12 +1,12 @@
-import * as Format from '../Format/Format.js'
-import * as PrettierWorkerCommandType from '../PrettierWorkerCommandType/PrettierWorkerCommandType.js'
+import * as Command from '../Command/Command.js'
+import * as GetErrorResponse from '../GetErrorResponse/GetErrorResponse.js'
+import * as GetSuccessResponse from '../GetSuccessResponse/GetSuccessResponse.js'
 
-export const getResponse = async (method, params) => {
-  if (method === PrettierWorkerCommandType.Format) {
-    const uri = params[0]
-    const content = params[1]
-    const result = await Format.format(uri, content)
-    return result
+export const getResponse = async (message) => {
+  try {
+    const result = await Command.execute(message.method, ...message.params)
+    return GetSuccessResponse.getSuccessResponse(message, result)
+  } catch (error) {
+    return GetErrorResponse.getErrorResponse(message, error)
   }
-  return undefined
 }
