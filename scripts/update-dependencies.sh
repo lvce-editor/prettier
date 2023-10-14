7e9fb3f1bc0ea2928738e7f559a9326e044346c1
Le Vivilet <levivlet@pm.me>
2023-10-14 16:49:12 +0200

    update dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -29,6 +29,7 @@ function updateDependencies {
 updateDependencies &&
 cd packages/e2e && updateDependencies && cd ../../ &&
 cd packages/extension && updateDependencies && cd ../../ &&
+cd packages/prettier-worker && updateDependencies && cd ../../ &&
 
 echo "Great Success!"
 
