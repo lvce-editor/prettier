6f6c0628fe9c933ec6cdcbfc3d07bed26b7a556b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-25 16:05:35 +0100

    feature: use prettier from node_modules to make it easier to upgrade … (#79)

--- packages/prettier-worker/src/parts/PrettierModule/PrettierModule.js
+++ packages/prettier-worker/src/parts/PrettierModule/PrettierModule.js
@@ -1,35 +1,37 @@
 import * as PrettierModuleId from '../PrettierModuleId/PrettierModuleId.js'
 
+const pathPrefix = '../../../../../node_modules/prettier'
+
 const loadInternal = (moduleId) => {
   switch (moduleId) {
     case PrettierModuleId.PluginAcorn:
-      return import('../../../third_party/prettier-v3/plugins/acorn.mjs')
+      return import(`${pathPrefix}/plugins/acorn.mjs`)
     case PrettierModuleId.PluginAngular:
-      return import('../../../third_party/prettier-v3/plugins/angular.mjs')
+      return import(`${pathPrefix}/plugins/angular.mjs`)
     case PrettierModuleId.PluginBabel:
-      return import('../../../third_party/prettier-v3/plugins/babel.mjs')
+      return import(`${pathPrefix}/plugins/babel.mjs`)
     case PrettierModuleId.PluginEstree:
-      return import('../../../third_party/prettier-v3/plugins/estree.mjs')
+      return import(`${pathPrefix}/plugins/estree.mjs`)
     case PrettierModuleId.PluginFlow:
-      return import('../../../third_party/prettier-v3/plugins/flow.mjs')
+      return import(`${pathPrefix}/plugins/flow.mjs`)
     case PrettierModuleId.PluginGlimmer:
-      return import('../../../third_party/prettier-v3/plugins/glimmer.mjs')
+      return import(`${pathPrefix}/plugins/glimmer.mjs`)
     case PrettierModuleId.PluginGraphql:
-      return import('../../../third_party/prettier-v3/plugins/graphql.mjs')
+      return import(`${pathPrefix}/plugins/graphql.mjs`)
     case PrettierModuleId.PluginHtml:
-      return import('../../../third_party/prettier-v3/plugins/html.mjs')
+      return import(`${pathPrefix}/plugins/html.mjs`)
     case PrettierModuleId.PluginMarkdown:
-      return import('../../../third_party/prettier-v3/plugins/markdown.mjs')
+      return import(`${pathPrefix}/plugins/markdown.mjs`)
     case PrettierModuleId.PluginMeriyah:
-      return import('../../../third_party/prettier-v3/plugins/meriyah.mjs')
+      return import(`${pathPrefix}/plugins/meriyah.mjs`)
     case PrettierModuleId.PluginPostCss:
-      return import('../../../third_party/prettier-v3/plugins/postcss.mjs')
+      return import(`${pathPrefix}/plugins/postcss.mjs`)
     case PrettierModuleId.PluginTypeScript:
-      return import('../../../third_party/prettier-v3/plugins/typescript.mjs')
+      return import(`${pathPrefix}/plugins/typescript.mjs`)
     case PrettierModuleId.PluginYaml:
-      return import('../../../third_party/prettier-v3/plugins/yaml.mjs')
+      return import(`${pathPrefix}/plugins/yaml.mjs`)
     case PrettierModuleId.Standalone:
-      return import('../../../third_party/prettier-v3/standalone.mjs')
+      return import(`${pathPrefix}/standalone.mjs`)
     default:
       throw new Error(`module ${moduleId} not found`)
   }
