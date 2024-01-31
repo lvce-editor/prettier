5d6228c1c95a82b667b23d3eb84fb5d71e8195ff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-01-31 21:57:00 +0100

    feature: replace dynamic imports with normal imports which simplifies… (#89)

--- packages/prettier-worker/src/parts/PluginModule/PluginModule.js
+++ packages/prettier-worker/src/parts/PluginModule/PluginModule.js
@@ -1,3 +1,18 @@
+import * as PluginCss from '../PluginCss/PluginCss.js'
+import * as PluginGraphql from '../PluginGraphql/PluginGraphql.js'
+import * as PluginHtml from '../PluginHtml/PluginHtml.js'
+import * as PluginJavaScript from '../PluginJavaScript/PluginJavaScript.js'
+import * as PluginJavaScriptReact from '../PluginJavaScriptReact/PluginJavaScriptReact.js'
+import * as PluginJson from '../PluginJson/PluginJson.js'
+import * as PluginJsonc from '../PluginJsonc/PluginJsonc.js'
+import * as PluginLess from '../PluginLess/PluginLess.js'
+import * as PluginMarkdown from '../PluginMarkdown/PluginMarkdown.js'
+import * as PluginScss from '../PluginScss/PluginScss.js'
+import * as PluginTypeScript from '../PluginTypeScript/PluginTypeScript.js'
+import * as PluginTypeScriptReact from '../PluginTypeScriptReact/PluginTypeScriptReact.js'
+import * as PluginVue from '../PluginVue/PluginVue.js'
+import * as PluginYaml from '../PluginYaml/PluginYaml.js'
+
 const extName = (uri) => {
   return uri.slice(uri.lastIndexOf('.'))
 }
@@ -6,34 +21,34 @@ export const loadPlugin = (uri) => {
   const extension = extName(uri)
   switch (extension) {
     case '.css':
-      return import('../PluginCss/PluginCss.js')
+      return PluginCss
     case '.gql':
-      return import('../PluginGraphql/PluginGraphql.js')
+      return PluginGraphql
     case '.html':
-      return import('../PluginHtml/PluginHtml.js')
+      return PluginHtml
     case '.js':
     case '.mjs':
-      return import('../PluginJavaScript/PluginJavaScript.js')
+      return PluginJavaScript
     case '.jsx':
-      return import('../PluginJavaScriptReact/PluginJavaScriptReact.js')
+      return PluginJavaScriptReact
     case '.json':
-      return import('../PluginJson/PluginJson.js')
+      return PluginJson
     case '.jsonc':
-      return import('../PluginJsonc/PluginJsonc.js')
+      return PluginJsonc
     case '.less':
-      return import('../PluginLess/PluginLess.js')
+      return PluginLess
     case '.md':
-      return import('../PluginMarkdown/PluginMarkdown.js')
+      return PluginMarkdown
     case '.scss':
-      return import('../PluginScss/PluginScss.js')
+      return PluginScss
     case '.ts':
-      return import('../PluginTypeScript/PluginTypeScript.js')
+      return PluginTypeScript
     case '.tsx':
-      return import('../PluginTypeScriptReact/PluginTypeScriptReact.js')
+      return PluginTypeScriptReact
     case '.vue':
-      return import('../PluginVue/PluginVue.js')
+      return PluginVue
     case '.yml':
-      return import('../PluginYaml/PluginYaml.js')
+      return PluginYaml
     default:
       throw new Error(`cannot format ${uri}`)
   }
