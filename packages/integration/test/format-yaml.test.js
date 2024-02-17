a8177aa2484b35df73bb2f588d02d42e1254f6b6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-17 10:30:13 +0100

    feature: add yaml integration test (#102)

---
+++ packages/integration/test/format-yaml.test.js
@@ -0,0 +1,15 @@
+import { testWorker } from '../src/testWorker.js'
+
+test('format yaml', async () => {
+  const execMap = {}
+  const worker = await testWorker({
+    execMap,
+  })
+  const uri = '/test/file.yml'
+  const content = '- x: 1'
+  expect(await worker.execute('Prettier.format', uri, content)).toEqual({
+    startOffset: 6,
+    endOffset: 6,
+    inserted: `\n`,
+  })
+})
