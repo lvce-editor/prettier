431b6644356c9c21b8926c563bb894a42f311e3b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-17 10:28:50 +0100

    feature: add json integration test (#101)

---
+++ packages/integration/test/format-json.test.js
@@ -0,0 +1,15 @@
+import { testWorker } from '../src/testWorker.js'
+
+test('format json', async () => {
+  const execMap = {}
+  const worker = await testWorker({
+    execMap,
+  })
+  const uri = '/test/file.json'
+  const content = '{}'
+  expect(await worker.execute('Prettier.format', uri, content)).toEqual({
+    startOffset: 2,
+    endOffset: 2,
+    inserted: `\n`,
+  })
+})
