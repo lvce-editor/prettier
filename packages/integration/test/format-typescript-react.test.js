96e2ab71adbeab016b91eb75da71687358ef5fd4
Le Vivilet <levivlet@pm.me>
2024-02-17 10:35:34 +0100

    feature: add typescript react integration test

---
+++ packages/integration/test/format-typescript-react.test.js
@@ -0,0 +1,15 @@
+import { testWorker } from '../src/testWorker.js'
+
+test('format typescript react', async () => {
+  const execMap = {}
+  const worker = await testWorker({
+    execMap,
+  })
+  const uri = '/test/file.tsx'
+  const content = ` let x: string = ""`
+  expect(await worker.execute('Prettier.format', uri, content)).toEqual({
+    startOffset: 0,
+    endOffset: 19,
+    inserted: 'let x: string = "";\n',
+  })
+})
