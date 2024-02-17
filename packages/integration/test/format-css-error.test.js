92825a6dfe56ae7ac67421660e9548560333882b
Le Vivilet <levivlet@pm.me>
2024-02-17 10:38:14 +0100

    feature: add css syntax error integration test

---
+++ packages/integration/test/format-css-error.test.js
@@ -0,0 +1,15 @@
+import { testWorker } from '../src/testWorker.js'
+
+test('format css', async () => {
+  const execMap = {}
+  const worker = await testWorker({
+    execMap,
+  })
+  const uri = '/test/file.css'
+  const content = ' h1 { '
+  await expect(worker.execute('Prettier.format', uri, content)).rejects.toThrow(
+    new Error(`Failed to format /test/file.css: SyntaxError: CssSyntaxError: Unclosed block (1:2)
+> 1 |  h1 { 
+    |  ^`),
+  )
+})
