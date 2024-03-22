687a77022ed8be38f4b24d7349fc6dc4c774ccf7
Le Vivilet <levivlet@pm.me>
2024-03-22 19:15:41 +0100

    fix build

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -4,7 +4,7 @@ import path, { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const root = path.join(__dirname, '..', '..', '..', '..')
+const root = path.join(__dirname, '..', '..', '..')
 
 await import('./build.js')
 
