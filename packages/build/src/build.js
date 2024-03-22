16f63260ff2b633c87aaa24aab0b84a1b5bc7505
Le Vivilet <levivlet@pm.me>
2024-03-22 20:40:45 +0100

    refactor: move bundlejs function into separate file

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -2,6 +2,7 @@ import { packageExtension } from '@lvce-editor/package-extension'
 import fs, { readFileSync, writeFileSync } from 'node:fs'
 import path, { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { bundleJs } from './bundle-js.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -112,42 +113,6 @@ replace({
   occurrence: '../../../../../node_modules/prettier',
   replacement: '../third_party/prettier',
 })
-
-const bundleJs = async (input, outFile) => {
-  const { babel } = await import('@rollup/plugin-babel')
-  const { default: pluginTypeScript } = await import('@babel/preset-typescript')
-  const { rollup } = await import('rollup')
-
-  const prettierWorkerOutput = await rollup({
-    input,
-    preserveEntrySignatures: 'strict',
-    treeshake: {
-      propertyReadSideEffects: false,
-    },
-    plugins: [
-      babel({
-        babelHelpers: 'bundled',
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        presets: [pluginTypeScript],
-      }),
-    ],
-  })
-
-  await prettierWorkerOutput.write({
-    file: outFile,
-    format: 'es',
-    sourcemap: true,
-    sourcemapExcludeSources: true,
-    inlineDynamicImports: true,
-    freeze: false,
-    minifyInternalExports: false,
-    generatedCode: {
-      constBindings: true,
-      objectShorthand: true,
-    },
-    hoistTransitiveImports: false,
-  })
-}
 
 await bundleJs(
   join(root, 'dist', 'prettier-worker', 'src', 'prettierWorkerMain.ts'),
