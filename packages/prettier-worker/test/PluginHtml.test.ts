3469c72dc11a8d70e5db6253e2313f7e41518db3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-03-24 19:44:47 +0100

    feature: don't rely on globals for prettier-worker tests (#141)

--- packages/prettier-worker/test/PluginHtml.test.ts
+++ packages/prettier-worker/test/PluginHtml.test.ts
@@ -1,6 +1,7 @@
 import * as PluginHtml from '../src/parts/PluginHtml/PluginHtml.ts'
 import * as Prettier from '../src/parts/Prettier/Prettier.ts'
 import * as PrettierModule from '../src/parts/PrettierModule/PrettierModule.ts'
+import { test, expect } from '@jest/globals'
 
 const plugins = await PrettierModule.loadAll(PluginHtml.plugins)
 const format = (code) => {
