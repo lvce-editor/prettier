37ec6e0b3906a386f932de90b1b0ad8fd0c4e42e
Le Vivilet <levivlet@pm.me>
2024-04-02 22:40:24 +0200

    feature: simplify build with commitHash variable

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,8 +1,8 @@
 import { exportStatic } from '@lvce-editor/shared-process'
-import { cp, readdir } from 'node:fs/promises'
+import { cp } from 'node:fs/promises'
 import path from 'node:path'
-import { root } from './root.js'
 import { replace } from './replace.js'
+import { root } from './root.js'
 
 await import('./build.js')
 
@@ -11,19 +11,11 @@ await cp(path.join(root, 'dist'), path.join(root, 'dist2'), {
   force: true,
 })
 
-await exportStatic({
+const { commitHash } = await exportStatic({
   extensionPath: 'packages/extension',
   testPath: 'packages/e2e',
   root,
 })
-
-const RE_COMMIT_HASH = /^[a-z\d]+$/
-const isCommitHash = (dirent) => {
-  return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
-}
-
-const dirents = await readdir(path.join(root, 'dist'))
-const commitHash = dirents.find(isCommitHash) || ''
 
 await cp(
   path.join(root, 'dist2'),
