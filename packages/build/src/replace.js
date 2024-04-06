c5e81bd758b0744183b301abae5637dbbd23760c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-06 18:50:32 +0200

    feature: simplify build replace function (#148)

--- packages/build/src/replace.js
+++
@@ -1,7 +0,0 @@
-import { readFileSync, writeFileSync } from 'node:fs'
-
-export const replace = ({ path, occurrence, replacement }) => {
-  const oldContent = readFileSync(path, 'utf8')
-  const newContent = oldContent.replace(occurrence, replacement)
-  writeFileSync(path, newContent)
-}
