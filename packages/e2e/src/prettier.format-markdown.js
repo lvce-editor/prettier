23b6541c2549defadf257c71b2a573204364ff3f
Le Vivilet <levivlet@pm.me>
2024-10-06 14:50:06 +0200

    feature: add markdown formatting e2e test

---
+++ packages/e2e/src/prettier.format-markdown.js
@@ -0,0 +1,15 @@
+export const name = 'prettier.format-markdown'
+
+export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.md`, ` # test`)
+  await Main.openUri(`${tmpDir}/test.md`)
+
+  // act
+  await Editor.format()
+
+  // assert
+  const editor = Locator('.Editor')
+  await expect(editor).toHaveText('# test')
+}
