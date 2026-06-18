d51fa64551c9b607c428b67d44a89e1dd1295cb2
Le Vivilet <levivilet@pm.me>
2026-06-18 23:47:08 +0200

    Build extension before e2e tests

---
+++ packages/build/src/build-extension.js
@@ -0,0 +1,26 @@
+import * as esbuild from 'esbuild'
+import fs from 'node:fs'
+import path from 'node:path'
+import { root } from './root.js'
+
+const extension = path.join(root, 'packages', 'extension')
+const entryPoint = path.join(extension, 'src', 'prettierMain.ts')
+const outdir = path.join(extension, 'dist')
+const outfile = path.join(outdir, 'prettierMain.js')
+
+fs.rmSync(outdir, { recursive: true, force: true })
+fs.mkdirSync(outdir, { recursive: true })
+
+await esbuild.build({
+  bundle: true,
+  define: {
+    PRETTIER_PATH_PREFIX: JSON.stringify('../../../node_modules/prettier'),
+  },
+  entryPoints: [entryPoint],
+  external: ['electron', 'node:*'],
+  format: 'esm',
+  outfile,
+  platform: 'browser',
+  sourcemap: true,
+  target: 'esnext',
+})
