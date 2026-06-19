939ace6360cd5fbe9ce18d9bd1cd242f4bf93824
Le Vivilet <levivilet@pm.me>
2026-06-19 22:25:36 +0200

    feature: enable ignore e2e tests

--- packages/e2e/src/prettier.ignore-multiple-files.ts
+++ packages/e2e/src/prettier.ignore-multiple-files.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'prettier.ignore-multiple-files'
 
-export const skip = 1
+// export const skip = 1
 
 export const test: Test = async ({
   Editor,
--- packages/e2e/src/prettier.ignore-root-file-glob.ts
+++ packages/e2e/src/prettier.ignore-root-file-glob.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'prettier.ignore-root-file-glob'
 
-export const skip = 1
+// export const skip = 1
 
 export const test: Test = async ({
   Editor,
