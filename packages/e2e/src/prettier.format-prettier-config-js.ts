9602882a1ded881bf6d2b59df93099082e9d7e66
Le Vivilet <levivilet@pm.me>
2026-06-19 11:16:29 +0200

    test: add formatting tests for package.json, .prettierrc, and prettier.config.js (#227)

---
+++ packages/e2e/src/prettier.format-prettier-config-js.ts
@@ -0,0 +1,24 @@
+export const name = "prettier.format-prettier-config-js";
+
+export const skip = 1;
+
+export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir();
+  await FileSystem.writeFile(
+    `${tmpDir}/prettier.config.js`,
+    `export default {
+  semi: false,
+  singleQuote: true,
+}`,
+  );
+  await FileSystem.writeFile(`${tmpDir}/test.js`, `let message="hello";`);
+  await Main.openUri(`${tmpDir}/test.js`);
+
+  // act
+  await Editor.format();
+
+  // assert
+  const editor = Locator(".Editor");
+  await expect(editor).toHaveText(`let message = 'hello'`);
+};
