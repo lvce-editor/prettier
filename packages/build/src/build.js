3c8994332cd397c6af22cdb51c68cbb94c59e256
Le Vivilet <levivilet@pm.me>
2026-06-19 15:24:52 +0200

    fix

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -1,7 +1,7 @@
 import { packageExtension } from '@lvce-editor/package-extension'
-import * as commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import fs from 'node:fs'
+import { createRequire } from 'node:module'
 import path, { join } from 'node:path'
 import { rollup } from 'rollup'
 import esbuild from 'rollup-plugin-esbuild'
@@ -9,6 +9,10 @@ import { copyPrettier } from './copyPrettier.js'
 import { root } from './root.js'
 
 const extension = path.join(root, 'packages', 'extension')
+const require = createRequire(import.meta.url)
+const commonjs = /** @type {() => import('rollup').Plugin} */ (
+  require('@rollup/plugin-commonjs')
+)
 
 fs.rmSync(join(root, 'dist'), { recursive: true, force: true })
 
@@ -29,7 +33,7 @@ const bundle = await rollup({
     nodeResolve({
       browser: true,
     }),
-    commonjs.default(),
+    commonjs(),
     esbuild({
       define: {
         PRETTIER_PATH_PREFIX: JSON.stringify('../third_party/prettier'),
