9b24fe56f02bd23d2c3a46289e9e73c1e42675d6
Le Vivilet <levivilet@pm.me>
2026-06-19 11:29:50 +0200

    refactor: clean up formatting and improve consistency in test files (#238)
    * refactor: clean up formatting and improve consistency in test files
    
    * fix

--- packages/e2e/src/prettier.format-package-json-config.ts
+++ packages/e2e/src/prettier.format-package-json-config.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'prettier.format-package-json-config'
 
-export const skip=1
+export const skip = 1
 
 export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
   // arrange
