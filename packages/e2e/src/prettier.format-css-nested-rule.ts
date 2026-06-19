5275cc380b360e8116a52ed1ad608ed88fd0b7f5
Le Vivilet <levivilet@pm.me>
2026-06-19 11:42:20 +0200

    feat: add formatting tests for various file types including CSS, GraphQL, HTML, JavaScript, Markdown, JSON, and YAML (#250)
    * feat: add formatting tests for various file types including CSS, GraphQL, HTML, JavaScript, Markdown, JSON, and YAML
    
    * test: skip failing prettier e2e cases

---
+++ packages/e2e/src/prettier.format-css-nested-rule.ts
@@ -0,0 +1,20 @@
+export const name = 'prettier.format-css-nested-rule'
+
+export const test = async ({ Editor, expect, FileSystem, Locator, Main }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/test.css`,
+    `@media (min-width:1px){h1{color:red}}`,
+  )
+  await Main.openUri(`${tmpDir}/test.css`)
+
+  // act
+  await Editor.format()
+
+  // assert
+  const editor = Locator('.Editor')
+  await expect(editor).toHaveText(
+    '@media (min-width: 1px) {  h1 {    color: red;  }}',
+  )
+}
