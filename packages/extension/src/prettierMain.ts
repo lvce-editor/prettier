fe01c8676f1404b4f992328fd63ee627b647aca8
Le Vivilet <levivilet@pm.me>
2026-06-19 11:34:24 +0200

    refactor: improve type definitions and consistency in worker interfaces

--- packages/extension/src/prettierMain.ts
+++ packages/extension/src/prettierMain.ts
@@ -1,4 +1,1 @@
 export { activate, deactivate } from './parts/Main/Main.ts'
-import { activate } from './parts/Main/Main.ts'
-
-await activate()
--- packages/integration/src/testWorker.ts
+++ packages/integration/src/testWorker.ts
@@ -3,15 +3,29 @@
 
 import { startWorker } from './startWorker.js'
 
+interface TestWorkerOptions {
+  readonly config?: Record<string, unknown>
+  readonly execMap?: Record<string, unknown>
+  readonly quickPick?: () => void
+}
+
+interface TestWorker {
+  readonly execute: (
+    commandId: string,
+    ...args: readonly unknown[]
+  ) => Promise<unknown>
+  readonly invocations: readonly unknown[]
+}
+
 export const testWorker = async ({
   config = {},
   execMap,
   quickPick = () => {},
-}) => {
-  const invocations: any[] = []
+}: TestWorkerOptions): Promise<TestWorker> => {
+  const invocations: unknown[] = []
   const worker = await startWorker()
   return {
-    execute(commandId: string, ...args: any[]) {
+    execute(commandId: string, ...args: readonly unknown[]): Promise<unknown> {
       return worker.execute(commandId, ...args)
     },
     invocations,
