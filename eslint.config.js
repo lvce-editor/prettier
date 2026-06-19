f1d9d6831dc7ba5facc9f9773bec0fa5eb2087c0
Le Vivilet <levivilet@pm.me>
2026-06-19 14:47:32 +0200

    feat: update eslint configuration to include additional rules (#257)

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,15 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [...config.default, ...actions.default]
+export default [
+  ...config.default,
+  ...actions.default,
+  {
+    rules: {
+      'github-actions/ci-versions': 'off',
+      'unicorn/no-top-level-side-effects': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      'no-useless-escape': 'off',
+    },
+  },
+]
