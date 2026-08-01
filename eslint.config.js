1187e2554b070938681dc7c810e91fab81bafc41
Le Vivilet <levivilet@pm.me>
2026-08-01 13:31:19 +0000

    feature: update eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [
+export default defineConfig([
   ...config.default,
-  ...actions.default,
+  ...config.recommendedActions,
   {
     rules: {
       'github-actions/ci-versions': 'off',
@@ -18,4 +18,4 @@ export default [
       'e2e/prefer-filesystem-set-files': 'off',
     },
   },
-]
+])
