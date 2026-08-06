84e6fa4d3c992bfaaad061a954337d1510a4768d
Le Vivilet <levivilet@pm.me>
2026-08-06 14:07:14 +0200

    feature: add react test

---
+++ packages/e2e/src/prettier.format-typescript-react.ts
@@ -0,0 +1,24 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'prettier.format-typescript-react-syntax-error'
+
+export const test: Test = async ({
+  Editor,
+  expect,
+  FileSystem,
+  Locator,
+  Main,
+}) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  const text = 'const  element: JSX.Element = <div></div>'
+  await FileSystem.writeFile(`${tmpDir}/test.tsx`, text)
+  await Main.openUri(`${tmpDir}/test.tsx`)
+
+  // act
+  await Editor.format()
+
+  // assert
+  const editor = Locator('.Editor')
+  await expect(editor).toHaveText('const element: JSX.Element = <div></div>')
+}
